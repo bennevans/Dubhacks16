7f93dde3905b3ba9eae14fe376d41b6cb5ba6751
Ben Evans <otterdude97@gmail.com>
2016-10-15 21:52:43 -0700

    added train_model, data directory

---
+++ train_model.py
@@ -0,0 +1,15 @@
+
+from clarifai.rest import ClarifaiApp
+import os
+
+CLIENT_ID = os.environ['CLARIFAI_CLIENT_ID']
+CLIENT_SECRET = os.environ['CLARIFAI_CLIENT_SECRET']
+
+print CLIENT_ID
+print CLIENT_SECRET
+
+#app = ClarifaiApp(CLIENT_ID, CLIENT_SECRET)
+
+
+
+
